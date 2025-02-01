7042566d11d5d4b59ade4fbe07dcd58b4c827e7b
Abdul Rahman <abdulnsamba@gmail.com>
2025-02-01 14:33:34 +0300

    added local storage and json

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -1,23 +1,14 @@
+// Retrieve elements from the DOM
 const inputText = document.getElementById("newQuoteText");
 const inputCat = document.getElementById("newQuoteCategory");
 const quoteDisplay = document.getElementById("quoteDisplay");
 const newQuote = document.getElementById("newQuote");
+const exportBtn = document.getElementById("exportQuotes");
 
-const quotes = [
-  {
-    text: "Kasbar is not recognised",
-    category: "Fun",
-  },
-  {
-    text: "Mo Salah is Ugandan",
-    category: "Sports",
-  },
-  {
-    text: "Math is a good language",
-    category: "Science",
-  },
-];
+// get quotes from the LocalStorage
+const quotes = initializeQuotes();
 
+// Show random quotes
 function showRandomQuote() {
   const rand = Math.floor(Math.random() * quotes.length);
   const randQuote = quotes[rand];
@@ -25,15 +16,53 @@ function showRandomQuote() {
   quoteDisplay.innerHTML = "";
   q.textContent = randQuote.text;
   quoteDisplay.appendChild(q);
+  sessionStorage.setItem("viewedQuote", randQuote);
 }
 
 newQuote.addEventListener("click", showRandomQuote);
 
+// Export Quotes to a json file
+exportBtn.addEventListener("click", function () {
+  const blob = new Blob([JSON.stringify(quotes, null, 2)], {
+    type: "application/json",
+  });
+  const url = URL.createObjectURL(blob);
+
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = "quotes.json";
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
+  console.log(url);
+});
+
+// Initialize quotes from localstorage
+function initializeQuotes() {
+  const availableQuotes = localStorage.getItem("quotes");
+  return availableQuotes ? JSON.parse(availableQuotes) : [];
+}
+
+// Create new Quote
 function createAddQuoteForm() {
-  quotes.push({
+  const fetchedQuotes = initializeQuotes();
+  fetchedQuotes.push({
     text: inputText.value,
     category: inputCat.value,
   });
+  localStorage.setItem("quotes", JSON.stringify(fetchedQuotes));
   inputText.value = "";
   inputCat.value = "";
+}
+
+// Import quotes from a json file.
+function importFromJsonFile(event) {
+  const fileReader = new FileReader();
+  fileReader.onload = function (event) {
+    const importedQuotes = JSON.parse(event.target.result);
+    quotes.push(...importedQuotes);
+    saveQuotes();
+    alert("Quotes imported successfully!");
+  };
+  fileReader.readAsText(event.target.files[0]);
 }
